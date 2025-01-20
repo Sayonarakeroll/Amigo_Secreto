7716bfc27169bebeda339055de35f478938d6ef4
sayonarakeroll <sayonarakerollyn@icloud.com>
2025-01-20 00:27:26 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let recebeAmigo = document.querySelector("input").value;
+  if (recebeAmigo) {
+    amigos.push(recebeAmigo);
+    atualizarListAmigos();
+    document.querySelector("input").value = "";
+  } else {
+    alert("Por favor, insira um nome");
+  }
+}
+
+function atualizarListAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    listaAmigos.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("A lista está vazia! Adicione amigos antes de sortear.");
+    return;
+  }
+  let nomeAleatorio = Math.floor(Math.random() * amigos.length);
+
+  let nomeDoSorteado = amigos[nomeAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `Amigo sorteado: ${nomeDoSorteado}`;
+}
